9165965db01832bfd1012ccb3ddd54b32f8b5f2c
Alex Christopher Ramos <alexchrisramos@Alex-Christopher-Ramos-MacBook-Pro.local>
2025-01-08 11:54:09 +0800

    test public repo

---
+++ debug.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Only an initial POC boiler plate script to test if we can
+# collect kubelet logs by deploying a daemonset and then
+# exec into each daemonset pods to collect the journalctl logs output
+
+# Deploy daemonset debugger-sh.yaml
+kubectl apply -f debugger-ds.yaml
+sleep 10
+
+# Get all debuggerds pods
+NODELOGDS=$(kubectl get pods -n castai-agent -l app=nodelog -o jsonpath='{.items[*].metadata.name}')
+
+# Loop through all nodes
+for POD in $NODELOGDS; do
+    echo "Pod: $POD"
+    kubectl exec -n castai-agent $POD -- chroot /host /bin/bash -c "journalctl -u kubelet" > $POD-kubelet.log
+done
+
+kubectl delete -f debugger-ds.yaml --now
